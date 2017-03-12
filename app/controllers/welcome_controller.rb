f766407e2bb3972afe1230b8ddc1e722f3145abf
PrestonPhelan <preston.phelan@gmail.com>
2017-03-11 18:35:45 -0800

    Test 3

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ require_relative '../../test_api_calls'
 
 class WelcomeController < ApplicationController
   def index
-    # welcome = "Welcome to my awesome app"
-      welcome = APIUtil.get_restaurants
+    welcome = "Welcome to my awesome app"
+      # welcome = APIUtil.get_restaurants
     	respond_with welcome
   end
 end
