90ee2036f8836cb171381acac3623ac1498f5497
elvynmejia <elvynmejia@gmail.com>
2017-03-12 00:42:48 -0800

    Adds authentication module for managing authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ require "application_responder"
 class ApplicationController < ActionController::API
   self.responder = ApplicationResponder
   respond_to :json
+  
+  protect_from_forgery with: :null_session
+
+  include Authenticable
 end
