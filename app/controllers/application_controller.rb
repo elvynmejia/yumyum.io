342abb2a3334b794c8f283d5b03f7a929c934f26
elvynmejia <elvynmejia@gmail.com>
2017-03-12 00:46:51 -0800

    Adds the authentication with token method to handle access to actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ require "application_responder"
 class ApplicationController < ActionController::API
   self.responder = ApplicationResponder
   respond_to :json
-  
-  protect_from_forgery with: :null_session
+
+  #protect_from_forgery with: :null_session
 
   include Authenticable
 end
