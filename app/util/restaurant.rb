97b6a9de7ca868dff2f56ed378958a0769c86654
PrestonPhelan <preston.phelan@gmail.com>
2017-03-12 10:56:35 -0700

    New Restaurant search method

--- app/util/restaurant.rb
+++ app/util/restaurant.rb
@@ -6,6 +6,18 @@ class Restaurant
 
   end
 
+  def self.find_by_lat_long(lat = 37.782039, long = -122.391139)
+    date = Date.today
+    time = "12:00"
+    party_size = 5
+    radius = 10
+
+    # Returns an array of restaurant objects
+    datetime = Util.format_datetime(date, time)
+    url = "https://platform.otqa.com/availability?latitude=#{lat}&longitude=#{long}&party_size=#{party_size}&radius=#{radius}&forward_minutes=180&backward_minutes=0&start_date_time=#{datetime}&include_unavailable=true"
+    APIUtil.open_table_get(url)
+  end
+
   def self.find_by_location(location = "San Francisco")
     ## Used as helper to find nearby bookings
     if location == "San Francisco"
