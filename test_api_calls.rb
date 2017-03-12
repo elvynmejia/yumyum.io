b7fadf9fa294eca13d0e9e9d96f423e17a4c137f
PrestonPhelan <preston.phelan@gmail.com>
2017-03-11 18:33:01 -0800

    Test 2

--- test_api_calls.rb
+++ test_api_calls.rb
@@ -7,7 +7,7 @@ class APIUtil
 
   def self.get_restaurants
     url = "https://platform.otqa.com/availability/334879?start_date_time=2017-03-29T18%3A00&party_size=2&forward_minutes=120&backward_minutes=30"
-    open_table_get(url)
+    APIUtil.open_table_get(url)
   end
 
 
@@ -15,7 +15,7 @@ class APIUtil
     # Returns an array of restaurant objects
     datetime = format_datetime(date, time)
     url = "https://platform.otqa.com/availability?latitude=#{lat}&longitude=#{long}&party_size=#{party_size}&radius=#{radius}&forward_minutes=15&backward_minutes=15&start_date_time=#{datetime}&include_unavailable=true"
-    open_table_get(url)
+    APIUtil.open_table_get(url)
   end
 
   def self.provisional_lock(size, date, time, restaurant_id)
@@ -27,11 +27,11 @@ class APIUtil
       date_time: datetime,
       reataurant_id: restaurant_id
     }
-    open_table_post(url, data)
+    APIUtil.open_table_post(url, data)
   end
 
   def self.make_reservation(data)
-    open_table_post("https://platform.otqa.com/booking/reservations", data)
+    APIUtil.open_table_post("https://platform.otqa.com/booking/reservations", data)
   #   Sample Request Body
   # {
   #   "restaurant_id": 123,
@@ -86,21 +86,21 @@ class APIUtil
 
   def self.get_reservation(restaurant_id, confirmation_number)
     url = "https://platform.opentable.com/booking/reservations/#{restaurant_id}-#{confirmation_number}"
-    open_table_get(url)
+    APIUtil.open_table_get(url)
   end
 
-  def open_table_get(url)
+  def self.open_table_get(url)
     HTTParty.get(url, headers: AUTH_HEADER)
   end
 
-  def open_table_post(url, data)
+  def self.open_table_post(url, data)
     headers = AUTH_HEADER.dup
     headers['Content-Type'] = 'application/json'
     HTTParty.post(url, body: data.to_json, headers: headers)
   end
 
 
-  def format_datetime(date, time)
+  def self.format_datetime(date, time)
     # TODO fix this
     "2017-03-13T20%3A00"
   end
