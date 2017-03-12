bfcfbaed72535f876fa6adddb56ffe540ea4f5fb
elvynmejia <elvynmejia@gmail.com>
2017-03-12 00:18:59 -0800

    Adds auth_token to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312071919) do
+ActiveRecord::Schema.define(version: 20170312075431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "bookings", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "confirmation_number"
-    t.string   "message"
-    t.datetime "date_time"
-    t.integer  "party_size"
-    t.integer  "restaurant_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-  end
-
-  add_index "bookings", ["user_id"], name: "index_bookings_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "user_name"
@@ -43,10 +30,11 @@ ActiveRecord::Schema.define(version: 20170312071919) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "auth_token",             default: ""
   end
 
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "bookings", "users"
 end
