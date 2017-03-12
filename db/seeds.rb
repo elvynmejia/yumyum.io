78ecd715ddf34797965112b246448e26bf45b164
elvynmejia <elvynmejia@gmail.com>
2017-03-12 10:22:31 -0700

    Adds bookings seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@ User.create(email: 'openseat5@openseat.com', password: 'openseat123', password_c
 User.create(email: 'openseat6@openseat.com', password: 'openseat123', password_confirmation: 'openseat123')
 
 Booking.create(user: user1, confirmation_number: 1234, message: "OpenSeat", date_time: Time.zone.now + 2.hour, party_size: 6, restaurant_id: 12345)
+Booking.create(user: user1, confirmation_number: 1234, message: "OpenSeat", date_time: Time.zone.now + 2.hour, party_size: 6, restaurant_id: 12345)
+Booking.create(user: user1, confirmation_number: 1234, message: "OpenSeat", date_time: Time.zone.now + 2.hour, party_size: 6, restaurant_id: 12345)
+Booking.create(user: user1, confirmation_number: 1234, message: "OpenSeat", date_time: Time.zone.now + 2.hour, party_size: 6, restaurant_id: 12345)
+Booking.create(user: user1, confirmation_number: 1234, message: "OpenSeat", date_time: Time.zone.now + 2.hour, party_size: 6, restaurant_id: 12345)
+Booking.create(user: user1, confirmation_number: 1234, message: "OpenSeat", date_time: Time.zone.now + 2.hour, party_size: 6, restaurant_id: 12345)
